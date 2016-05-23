96fcdc45e5d68ba6e6da134397a7cdc6e195640c
Chris Dziewa <chris.dziewa@gmail.com>
2016-05-23 15:13:23 -0500

    Initialize repository

---
+++ app/db/index.js
@@ -0,0 +1,21 @@
+'use strict';
+const config = require('../config');
+const User = require('../models/user');
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect(config.dbURI, (err, db) => {
+  if (err) {
+    console.log('MongoDB Error: ', err);
+  } else {
+      console.log('Connected to \'db_name\' database');
+    }
+});
+
+// Turn schemas into a usable models
+let userModel = mongoose.model('User', User);
+
+module.exports = {
+  mongoose,
+  userModel
+}
